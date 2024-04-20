925b73547a7636a732698f2f25b9bd71547085b6
Gabriel Pires <gabrielpires2991@gmail.com>
2024-04-20 13:07:09 -0300

    Iniciando script sql

---
+++ script_hospital.sql
@@ -0,0 +1,90 @@
+create database hospital;
+
+create table interno(
+	cpf varchar(11) primary key,
+	nome varchar(100) not null,
+	senha varchar(20) not null,
+	matricula integer not null unique
+);
+
+create table paciente(
+	cpf varchar(11) primary key,
+	nome varchar(100) not null,
+	telefone_residencial varchar(25),
+	telefone_pessoal varchar(25) not null,
+	cidade varchar(50) not null,
+	bairro varchar(50) not null,
+	rua varchar(50) not null,
+	numero integer not null
+);
+
+create table monitora(
+	fk_cpf_interno VARCHAR(11) NOT NULL,
+    fk_cpf_paciente VARCHAR(11) NOT NULL,
+    primary key (fk_cpf_interno, fk_cpf_paciente),
+    foreign key (fk_cpf_interno) references interno(cpf),
+    foreign key (fk_cpf_paciente) references paciente(cpf)
+);
+
+create table paciente_internado(
+	fk_paciente_cpf varchar(11) not null,
+	sala integer not null,
+	primary key (fk_paciente_cpf),
+	foreign key (fk_paciente_cpf) references paciente(cpf)
+);
+
+create table paciente_urgencia(
+	fk_paciente_cpf varchar(11) not null,
+	nivel_triagem integer not null check (nivel_triagem between 0 and 4),
+	primary key (fk_paciente_cpf),
+	foreign key (fk_paciente_cpf) references paciente(cpf)
+);
+
+create table registro(
+	codigo integer primary key,
+	conduta varchar(50)
+);
+
+create table hipotese(
+	fk_registro_codigo integer not null,
+	id integer not null,
+	primary key (fk_registro_codigo, id),
+	foreign key (fk_registro_codigo) references registro(codigo)
+);
+
+create table registro_internado(
+	fk_registro_codigo integer not null,
+	evolucao varchar(500) not null,
+	historico_exames varchar(500) not null,
+	primary key(fk_registro_codigo),
+	foreign key (fk_registro_codigo) references registro(codigo)
+);
+
+create table registro_urgencia(
+	fk_registro_codigo integer not null,
+	historico_doenca varchar(500) not null,
+	exame_fisico varchar(500) not null,
+	primary key (fk_registro_codigo),
+	foreign key (fk_registro_codigo) references registro(codigo)
+);
+
+create table medicacao(
+	fk_registro_urgencia_codigo integer not null,
+	id integer,
+	nome varchar(100),
+	primary key (fk_registro_urgencia_codigo, id),
+	foreign key (fk_registro_urgencia_codigo) references registro_urgencia(fk_registro_codigo)
+);
+
+create table comorbidade(
+	fk_registro_urgencia_codigo integer not null,
+	id integer,
+	nome varchar(100),
+	primary key (fk_registro_urgencia_codigo, id),
+	foreign key (fk_registro_urgencia_codigo) references registro_urgencia(fk_registro_codigo)
+);
+
+commit;
+
+
+
