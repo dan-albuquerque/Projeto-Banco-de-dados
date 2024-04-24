6e5c15180ad824318d78cfc863a69ab7a10b7edd
CarlosAugustoP <capv2004@gmail.com>
2024-04-24 12:13:56 -0300

    progress

---
+++ bd-project/hospital/src/main/java/com/hospital/hospital/controllers/MonitoraController.java
@@ -0,0 +1,46 @@
+package com.hospital.hospital.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hospital.hospital.models.relacoes.Monitora;
+
+import com.hospital.hospital.repository.MonitoraRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/monitora")
+public class MonitoraController {
+
+    @Autowired
+    private MonitoraRepository monitoraRepository;
+
+    @PostMapping
+    public String createMonitora(@RequestBody Monitora monitora) {
+        monitoraRepository.insertMonitora(monitora);
+        return "Monitora created: " + monitora.getCpfInterno();
+    }
+
+    @DeleteMapping
+    public String deleteMonitora(@RequestBody Monitora monitora){
+        monitoraRepository.deleteMonitora(monitora);
+        return "Monitora deleted! ";
+    }
+
+    @GetMapping
+    public List<Monitora> getAllMonitoras() {
+        return monitoraRepository.selectMonitoras();
+    }
+
+    @PutMapping
+    public String updateMonitora(@RequestBody Monitora monitora){
+        monitoraRepository.updateMonitora(monitora);
+        return "Monitora updated: " + monitora.getCpfInterno();
+    }
+
+    
+}
--- bd-project/hospital/src/main/java/com/hospital/hospital/repository/MonitoraRepository.java
+++ bd-project/hospital/src/main/java/com/hospital/hospital/repository/MonitoraRepository.java
@@ -14,7 +14,8 @@ public class MonitoraRepository {
     private JdbcTemplate jdbcTemplate;
     
     public void insertMonitora(Monitora monitora){
-        jdbcTemplate.update("insert into monitora(fk_cpf_interno,fk_cpf_paciente) values(?, ?)", monitora.getCpfInterno(), monitora.getCpfPaciente());
+       System.out.println(monitora.getCpfInterno());
+       System.out.println(monitora.getCpfPaciente());
     }
 
     public List<Monitora> selectMonitoras() {
@@ -28,6 +29,14 @@ public class MonitoraRepository {
         monitora.setCpfInterno(rs.getString("fk_cpf_paciente"));
         return monitora;
     };
+
+    public void deleteMonitora(Monitora monitora){
+        jdbcTemplate.update("delete from monitora where fk_cpf_interno = ? and fk_cpf_paciente = ?", monitora.getCpfInterno(), monitora.getCpfPaciente());
+    }
+
+    public void updateMonitora(Monitora monitora){
+        jdbcTemplate.update("update monitora set fk_cpf_paciente = ? where fk_cpf_interno = ?", monitora.getCpfPaciente(), monitora.getCpfInterno());
+    }
 
     
     
