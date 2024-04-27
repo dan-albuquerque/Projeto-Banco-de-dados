f9ee66af61994ff2cada8294ca3de4d7722b4d12
Carlos Augusto <capv2004@gmail.com>
2024-04-27 12:55:48 -0300

    adding next project

---
+++ client/src/pages/intern.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export async function getServerSideProps(context) {
+    const res = await fetch(`http://localhost:8080/intern`)
+    const data = await res.json()
+
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { interns:data }, // will be passed to the page component as props
+    }
+
+}
+
+
+export default function Interns({ interns }) {
+    return (
+      <div>
+        <h1>Interns List</h1>
+        <ul>
+          {interns.map((intern) => (
+            <li key={intern.cpf}>
+              Name: {intern.nome}, CPF: {intern.cpf}, Password: {intern.senha}, ID: {intern.matricula}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
