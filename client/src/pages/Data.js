17d4f5208724fdd917fe884587c496c85a60970b
Carlos Augusto <capv2004@gmail.com>
2024-05-01 15:44:50 -0300

    add sorted patient

--- client/src/pages/Data.js
+++ client/src/pages/Data.js
@@ -11,31 +11,36 @@ export async function getServerSideProps(context) {
   const urls = {
       interns: `http://localhost:8080/intern`,
       doctors: `http://localhost:8080/medico`,
-      patients: `http://localhost:8080/pacient`
+      patients: `http://localhost:8080/pacient`,
+      sortedPatients: `http://localhost:8080/pacient?sort=alphabetical`
   };
 
   try {
-      const [internsRes, doctorsRes, patientsRes] = await Promise.all([
+      const [internsRes, doctorsRes, patientsRes, sortedPatientsRes] = await Promise.all([
           fetch(urls.interns),
           fetch(urls.doctors),
-          fetch(urls.patients)
+          fetch(urls.patients),
+          fetch(urls.sortedPatients)
+
       ]);
 
-      const [interns, doctors, patients] = await Promise.all([
+      const [interns, doctors, patients,  sortedPatients] = await Promise.all([
           internsRes.json(),
           doctorsRes.json(),
-          patientsRes.json()
+          patientsRes.json(),
+          sortedPatientsRes.json()
       ]);
 
-      if (!interns || !doctors || !patients) {
-          return { notFound: true };
+      if (!interns || !doctors || !patients || !sortedPatients) {  
+        return { notFound: true };
       }
 
       return {
           props: {
               interns,
               doctors,
-              patients
+              patients,
+              sortedPatients
           },
       };
   } catch (error) {
@@ -44,7 +49,7 @@ export async function getServerSideProps(context) {
   }
 }
 
-export default function Data({ interns, doctors, patients}) {
+export default function Data({ interns, doctors, patients, sortedPatients}) {
   const [isIntern, setIsIntern] = useState(true);
   const [isDoctor, setIsDoctor] = useState(false);
   const [isPatient, setIsPatient] = useState(false);
@@ -77,50 +82,23 @@ export default function Data({ interns, doctors, patients}) {
       setIsSortedPatients(false);
     } else {
       setIsSortedPatients(true);
-      getSortedPacients();
     }
     console.log(isSortedPatients);
   };
 
-  async function getSortedPacients() {
-    const urls = {
-        sortedPatients: `http://localhost:8080/pacient?sort=alphabetical`
-    };
-
-    try {
-        const [sortedPatientsRes] = await Promise.all([
-            fetch(urls.sortedPatients)
-        ]);
-
-        const [sortedPatients] = await Promise.all([
-          sortedPatientsRes.json()
-        ]);
-
-        if (!sortedPatients) {
-            return { notFound: true };
-        }
-
-        return {
-            props: {
-              sortedPatients
-            },
-        };
-    } catch (error) {
-        console.error("Failed to fetch data:", error);
-        return { notFound: true };
-    }
-  }
     
 
   const renderTable = () => {
     if (isIntern) {
         return <InternTableView interns={interns} />;
     } else if (isDoctor) {
-        return <DoctorsTableView doctors={doctors} />;  // Assuming similar component can be used or create separate ones
-    } else if (isPatient) {
+        return <DoctorsTableView doctors={doctors} />;  // Assuming similar component can be used or create separate ones      
+    } else if (isPatient)  {
+        console.log(isSortedPatients);
+        if (isSortedPatients) {
+          return <PatientTableView patients={sortedPatients} />;
+        }
         return <PatientTableView patients={patients} />;
-    }else if (isSortedPatients && isPatient) {
-        return <PatientTableView patients={sortedPatients} />;
     }
 };
 
