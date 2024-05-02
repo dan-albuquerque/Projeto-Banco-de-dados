3125e51df376ca5dacf7e78cce3b15e4edf9a64c
Carlos Augusto <capv2004@gmail.com>
2024-05-02 16:37:17 -0300

    improve styles

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,12 +2,14 @@ import React from  'react';
 
 export default function SearchBar({searchQuery, setSearchQuery}) {
     return (
-            <input
+        <div className='w-1/3 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 flex justify-between' > {/* Wrap JSX elements inside a parent element */}
+            <input className='w-full bg-transparent focus:outline-none'
                 type="text"
-                className="w-1/3 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600"
                 placeholder="Pesquisar..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
             />
+            <img src="/img/search.png" alt="search" className="w-6 h-6 ml-2" /> {/* Remove the closing slash */}
+        </div>
     );
 }
