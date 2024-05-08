6ba3de7f915cc2297cc74b4d51df84596da8700a
Carlos Augusto <capv2004@gmail.com>
2024-05-08 20:25:32 -0300

    add profile viewing user story

--- client/src/pages/Perfil.js
+++ client/src/pages/Perfil.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
-
+import { CentralizedLayout } from '@/app/layout';
+import DownerNav from '@/components/DownerNav';
 export default function Perfil() {
   const [medico, setMedico] = useState({
     cpf: "",
@@ -54,18 +55,39 @@ export default function Perfil() {
   }, [cpfGerente]);
 
   return (
-    <>
+    <CentralizedLayout>
       <Toaster />
-      <div>
-        <h1>Perfil do Médico</h1>
-        <p>CPF: {medico.cpf}</p>
-        <p>Nome: {medico.nome}</p>
-        <p>RQE: {medico.rqe}</p>
-        <p>Especialidade: {medico.especialidade}</p>
-        <p>CRM: {medico.crm}</p>  
-        {medico.fk_medico_cpf_gerente && <p>Gerente: {nomeGerente}</p>}
-        {medico.fk_medico_cpf_gerente === null && <p>Cargo: gerente</p>}
+      <div className='w-4/4 flex justify-between gap-10' style = {{height: '50vh'}}>
+      <div className='w-1/2 h-full'>
+          <div className='h-full w-full rounded-full bg-blue-200 border-customBlue border-8'>
+            <img className='w-full h-full object-cover rounded-full' src='img/doctor.svg' alt='Foto do médico' />
+           </div> 
+        </div>
+        <div className='flex items-start w-1/2 flex-col gap-20'>
+          <div className='gap-1'>
+            <h1 className= 'text-7xl'>{medico.nome}</h1>
+            <p className='italic text-4xl text-gray-600'>{medico.especialidade}</p>
+          </div>
+          <div className='flex flex-col gap-3'>
+            <div className='flex gap-4 text-3xl'>
+              <p><b>CRM:</b> {medico.crm}</p>
+              <p><b>RQE:</b> {medico.rqe}</p>
+            </div>
+            <div className='flex flex-col'>
+                {medico.fk_medico_cpf_gerente && <p className='text-3xl'><b>Gerente:</b> {nomeGerente}</p>}
+                {medico.fk_medico_cpf_gerente === null && <p className='text-3xl'><b>Cargo:</b> gerente</p>}
+            </div>
+            
+          </div>
+          <div className='flex gap-6'>
+              <button className='bg-black text-white text-xl rounded-md p-3 shadow-md transition-transform duration-200 hover:scale-105'>Ver pacientes</button>
+              <button className='bg-black text-white text-xl rounded-md p-3 shadow-md transition-transform duration-200 hover:scale-105'>Ver Consultas</button>
+              <button className='bg-black text-white text-xl rounded-md p-3 shadow-md transition-transform duration-200 hover:scale-105 flex gap-4 items-center justify-center'><p>Sair</p><img className= "w-7 h-7"src = "img/logout2.svg"></img></button>
+            </div>
+        </div>
+        
       </div>
-    </>
+      <DownerNav />
+    </CentralizedLayout>
   );
 }
