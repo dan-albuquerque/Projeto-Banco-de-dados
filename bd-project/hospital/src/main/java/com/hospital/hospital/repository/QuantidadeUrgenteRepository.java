e4517d69b0c605cc543195357c7bd28dce091e41
Gabriel Pires <gabrielpires2991@gmail.com>
2024-05-23 09:01:13 -0300

    remove: removendo arquivos

--- bd-project/hospital/src/main/java/com/hospital/hospital/repository/QuantidadeUrgenteRepository.java
+++ bd-project/hospital/src/main/java/com/hospital/hospital/repository/QuantidadeUrgenteRepository.java
@@ -1,7 +1,6 @@
 package com.hospital.hospital.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.hospital.hospital.models.DTOs.QuantidadeCasosUrgenciaDTO;
 import org.springframework.jdbc.core.JdbcTemplate;
 @Repository
